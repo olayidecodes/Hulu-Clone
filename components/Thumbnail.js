8b0b4e96e445bf4168a4990201356bb49ad58de6
olayidecodes <olayidecodes@gmail.com>
2022-08-09 17:08:49 +0100

    final commit

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -9,7 +9,7 @@ const Thumbnail = forwardRef(({result}, ref) => {
     <div ref={ref} className='p-2 group cursor-pointer 
     transition duration-200 ease-in transform 
     sm:hover:scale-105 hover:z-50'>
-        <Image 
+        <Image  
             layout='responsive'
             src={
                 `${BASE_URL}${result.backdrop_path || result.poster_path}` || 
