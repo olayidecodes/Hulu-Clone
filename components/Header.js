fdee4b2544ef458b8220161db5488248d95ad1fe
olayidecodes <olayidecodes@gmail.com>
2022-08-09 17:31:01 +0100

    final commit

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
             <HeaderItem title="SEARCH" Icon={SearchIcon} />
             <HeaderItem title="ACCOUNT" Icon={UserIcon} />
         </div>
-        <Image className='object-contain' src="https://links.papareact.com/ua6" width={200} height={100}/>
+        <Image className='object-contain' src="https://links.papareact.com/ua6" width={200} height={100} alt="logo"/>
     </header>
   )
 }
